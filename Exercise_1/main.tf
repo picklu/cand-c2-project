72ef5f35b6eb3f0b1e3f006e39a96a2f4a21f414
Subrata Sarker <subrata_sarker@yahoo.com>
2021-09-27 15:42:43 +0900

    Format the file according to terraform style guide

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,28 +1,28 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
+    profile = "udacity"
     region  = "us-east-1"
-    profile = "udacity" 
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-resource "aws_instance" "Udacity T2" {
-    count = 4
-    ami = "ami-0323c3dd2da7fb37d"
+resource "aws_instance" "UdacityT2" {
+    count         = 4
+    ami           = "ami-0323c3dd2da7fb37d"
     instance_type = "t2.micro"
     
     tags = {
-            Name  = "EC2-Udacity-t2_micro-${count.index + 1}"
+            Name = "Udacity T2_${count.index + 1}"
         }
 }
 
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-resource "aws_instance" "Udacity M4" {
-    count = 2
-    ami = "ami-0323c3dd2da7fb37d"
-    instance_type = "m4.large"
+resource "aws_instance" "UdacityM4" {
+    count          = 2
+    ami            = "ami-0323c3dd2da7fb37d"
+    instance_type  = "m4.large"
     
     tags = {
-            Name  = "EC2-Udacity-m4_large-${count.index + 1}"
+            Name = "Udacity M4_${count.index + 1}"
         }
 }
