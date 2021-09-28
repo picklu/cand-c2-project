14fe782a9da62ac9177e27894372c96c6118c4c6
Subrata Sarker <subrata_sarker@yahoo.com>
2021-09-28 22:32:22 +0900

    Update the script for deployment of lambda function

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -1,53 +1,45 @@
-# Designate a cloud provider, region, and credentials
 provider "aws" {
-  profile = "udacity"
-  region  = "us-east-1"
+  region = var.aws_region
 }
 
-// vpc to be imported
-resource "aws_vpc" "default-vpc" {
-  cidr_block = "172.31.0.0/16"
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "greet_lambda.py"
+  output_path = "greet_lambda.zip"
+}
 
-  tags = {
-    Name = "udacity-vpc"
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
   }
 }
 
-# subnet to be imported
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.default-vpc.id
-  cidr_block              = "172.31.32.0/20"
-  map_public_ip_on_launch = true
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
 }
 
-# subnet to be imported
-resource "aws_subnet" "public_subnet2" {
-  vpc_id                  = aws_vpc.default-vpc.id
-  cidr_block              = "172.31.16.0/20"
-  map_public_ip_on_launch = true
-}
+resource "aws_lambda_function" "greet_lambda" {
+  function_name = "hello_lambda"
 
-# provision 4 AWS t2.micro EC2 instances named Udacity T2
-resource "aws_instance" "UdacityT2" {
-  count         = 4
-  ami           = "ami-0323c3dd2da7fb37d"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
 
-  tags = {
-    Name = "Udacity T2_${count.index + 1}"
+  role    = aws_iam_role.lambda_role.arn
+  handler = "greet_lambda.lambda_handler"
+  runtime = "python3.8"
+
+  environment {
+    variables = {
+      greeting = "Hello"
+    }
   }
 }
-
-
-# provision 2 m4.large EC2 instances named Udacity M4
-# resource "aws_instance" "UdacityM4" {
-#   count         = 2
-#   ami           = "ami-0323c3dd2da7fb37d"
-#   instance_type = "m4.large"
-#   subnet_id     = aws_subnet.public_subnet2.id
-
-#   tags = {
-#     Name = "Udacity M4_${count.index + 1}"
-#   }
-# }
--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,4 @@
-# TODO: Define the output variable for the lambda function.
+# Output variable for the lambda function.
+output "lambda" {
+  value = aws_lambda_function.greet_lambda.qualified_arn
+}
