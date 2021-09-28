ac310a23ff1e333df6430e3aca82804570199cae
Subrata Sarker <subrata_sarker@yahoo.com>
2021-09-28 12:01:45 +0900

    Initial commit for Exercise_2 of project 2

---
+++ Exercise_2/main.tf
@@ -0,0 +1,53 @@
+# Designate a cloud provider, region, and credentials
+provider "aws" {
+  profile = "udacity"
+  region  = "us-east-1"
+}
+
+// vpc to be imported
+resource "aws_vpc" "default-vpc" {
+  cidr_block = "172.31.0.0/16"
+
+  tags = {
+    Name = "udacity-vpc"
+  }
+}
+
+# subnet to be imported
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.default-vpc.id
+  cidr_block              = "172.31.32.0/20"
+  map_public_ip_on_launch = true
+}
+
+# subnet to be imported
+resource "aws_subnet" "public_subnet2" {
+  vpc_id                  = aws_vpc.default-vpc.id
+  cidr_block              = "172.31.16.0/20"
+  map_public_ip_on_launch = true
+}
+
+# provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "UdacityT2" {
+  count         = 4
+  ami           = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "Udacity T2_${count.index + 1}"
+  }
+}
+
+
+# provision 2 m4.large EC2 instances named Udacity M4
+# resource "aws_instance" "UdacityM4" {
+#   count         = 2
+#   ami           = "ami-0323c3dd2da7fb37d"
+#   instance_type = "m4.large"
+#   subnet_id     = aws_subnet.public_subnet2.id
+
+#   tags = {
+#     Name = "Udacity M4_${count.index + 1}"
+#   }
+# }
